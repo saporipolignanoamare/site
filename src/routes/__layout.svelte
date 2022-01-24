e4750663a2b6296402c9d1eb40a32f35da616691
bbtgnn <bbt.gnn@gmail.com>
2022-01-24 19:10:33 +0100

    layout update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-	import { t, locale, locales, loadTranslations } from '$lib/translations';
+	import { locale, loadTranslations } from '$lib/translations';
 
 	export const load = async ({ url }) => {
 		const { pathname } = url;
@@ -11,92 +11,28 @@
 </script>
 
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { category } from '$lib/stores';
+	import { showMenu } from '$lib/stores';
 
-	import Home24 from 'carbon-icons-svelte/lib/Home24/Home24.svelte';
-
-	//
-
-	let otherLocale;
-	$: {
-		otherLocale = $locale == 'it' ? 'en' : 'it';
-	}
+	import Footer from '$lib/components/footer.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
+	import Menu from '$lib/components/menu.svelte';
 </script>
 
-<div class="navbar">
-	<div class="navbar-content max-width">
-		<!-- Left side -->
-		<div>
-			{#if $page.url.pathname != '/'}
-				<a href="/" class="home nav-button">
-					<Home24 style="fill: white" />
-				</a>
-			{/if}
-			{#if $category && $page.params.category}
-				<p class="category">{$category}</p>
-			{:else}
-				<p class="title">Sapori</p>
-			{/if}
-		</div>
+<!--  -->
 
-		<!-- Right side -->
-		<div>
-			<button
-				class="nav-button"
-				on:click={() => {
-					$locale = otherLocale;
-				}}>{$t(`lang.${otherLocale}`)}</button
-			>
-			<!-- menu button here -->
-		</div>
+<Navbar />
+{#if $showMenu}
+	<Menu />
+{:else}
+	<div>
+		<slot />
 	</div>
-</div>
-<slot />
+	<Footer />
+{/if}
 
+<!--  -->
 <style>
-	/* Containers */
-
-	.navbar {
-		width: 100%;
-		background-color: var(--accent);
-		position: sticky;
-		top: 0;
-		z-index: 99;
-		border-bottom: 1px solid rgba(255, 255, 255, 0.3);
-	}
-
-	.navbar-content {
-		padding: calc(var(--spacing) / 2) var(--spacing);
-		display: flex;
-		flex-flow: row nowrap;
-		justify-content: space-between;
-	}
-
-	/* Text */
-
-	.title,
-	.category {
-		color: white;
-		text-decoration: none;
-	}
-
-	.title {
-		font-size: xx-large;
-	}
-
-	.category {
-		font-size: x-large;
-	}
-
-	/* Buttons */
-
-	button {
-		font-size: 26px;
-		padding-top: 5px;
-	}
-
-	.home {
-		margin-right: 5px;
+	div {
+		background-color: white;
 	}
 </style>
