d020d942d156964cdf74e2320567a746808e017b
bbtgnn <bbt.gnn@gmail.com>
2022-01-25 19:55:01 +0100

    added menu icons

--- src/lib/components/menuLink.svelte
+++ src/lib/components/menuLink.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+
+	//
+
 	export let href: string;
+	export let icon: Function | null = null;
+
+	//
 	let isOpen: boolean = false;
 
 	$: if ($page.url.pathname == href) {
@@ -11,12 +17,18 @@
 </script>
 
 <a {href} class:open={isOpen} class:default={!isOpen}>
+	{#if icon}
+		<svelte:component this={icon} style="fill: {isOpen ? 'olive' : 'white'}; margin-right: 10px" />
+	{/if}
 	<slot />
 </a>
 
 <style>
 	a {
-		display: block;
+		display: flex;
+		flex-flow: row nowrap;
+		align-items: center;
+		justify-content: flex-start;
 		padding: 10px;
 		border-radius: 5px;
 		font-size: 20px;
