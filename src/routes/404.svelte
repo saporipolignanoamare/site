62a8801f7ce66c46b0758fe56e9c640c1bf8872d
bbtgnn <bbt.gnn@gmail.com>
2022-01-31 23:01:21 +0100

    rebuild III

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { browser } from '$app/env';
 </script>
 
 <pre>{JSON.stringify($page.params, null, 2)}</pre>
+{#if browser}
+	{window.location.href}
+{/if}
